1a870026c2b39793fa7125aec5ba99b086aef20a
KELVIN5671 <117321368+KELVIN5671@users.noreply.github.com>
2022-11-13 03:21:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ global.APIKeys = {
 }
 
 // NAMBAH BOLEH HAPUS JANGAN -_-
-global.owner = ['6281511480762']
-global.premium = ['621511480762']
+global.owner = ['6281523783262','6281511480762']
+global.premium = ['6281523783262','621511480762']
 global.packname = 'AzzBot√'
 global.author = 'AzzBot-MD'
 global.sessionName = 'Xavior'
